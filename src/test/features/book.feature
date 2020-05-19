ed70d383441807b25f58d20871dd485e3a760c36
Betania <betaniafa@hotmail.com>
2020-05-19 09:46:37 -0300

    versão final

--- src/test/features/book.feature
+++ src/test/features/book.feature
@@ -1,10 +1,11 @@
-Feature: Get book by ISBN
-#  Scenario: User calls web service to get a book by its ISBN
-#    Given a book exists with an isbn of x
-#    When a user retrieves the book by isbn
-#    Then the status code is x
+Feature: New book
 
   Scenario: Create a new book
-    Given a book exists with an isbn of x
-    When I post
-    Then the status code is x
+    Given an insertion of a book with id '01', title 'O Poder do Hábito', description 'O autor toca no outro assunto importantíssimo, que é a criação de hábitos corretos.', pageCount '408', excerpt 'Os hábitos surgem sem a nossa permissão.', PublishDate '2012-01-10T20:43:36.324Z'
+    When I post new book
+    Then the book with title 'O Poder do Hábito' was successfully created and statusCode return '200'
+
+  Scenario: Search a book
+    Given fetch active book
+    When fetch book with id '190'
+    Then the book with title 'Book 190' was successfully found
--- src/test/java/steps/MyStepdefs.java
+++ src/test/java/steps/MyStepdefs.java
@@ -7,70 +7,63 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.json.simple.JSONObject;
-import org.junit.Assert;
 
 
 public class MyStepdefs {
-    private Response response;
-    private ValidatableResponse json;
-    private RequestSpecification request;
-    private String ENDPOINT_GET_BOOK_BY_ISBN = "https://fakerestapi.azurewebsites.net/api/Books";
+    private Response responsePost;
+    private Response responseGet;
+    private RequestSpecification requestPost;
+    private RequestSpecification requestGet;
+    private String ENDPOINT_GET_BOOK = "https://fakerestapi.azurewebsites.net/api/Books";
 
-
-    @Given("a book exists with an isbn of x")
-    public void aBookExistsWithAnIsbnOfX() {
+    @Given("^an insertion of a book with id '(.*?)', title '(.*?)', description '(.*?)', pageCount '(.*?)', excerpt '(.*?)', PublishDate '(.*?)'$")
+    public void an_insertion_of_a_book_with_id_title_description_pageCount_excerpt_PublishDate(int id, String title, String description, String pageCount, String excerpt, String PublishDate) {
         JSONObject book = new JSONObject();
 
-        book.put("ID", 123);
-        book.put("Title", "Betania");
-        book.put("Description", "betania é d+");
-        book.put("PageCount", 200);
-        book.put("Excerpt", "Você é luz");
-        book.put("PublishDate", "2020-05-18T20:43:36.324Z");
+        book.put("ID", id);
+        book.put("Title", title);
+        book.put("Description", description);
+        book.put("PageCount", pageCount);
+        book.put("Excerpt", excerpt);
+        book.put("PublishDate", PublishDate);
 
-        request = RestAssured.given().
-                                request().
-                                header("Content-Type", "application/json").
-                                body(book.toJSONString());
-    }
-
-    @When("a user retrieves the book by isbn variavel")
-    public void a_user_retrieves_the_book_by_isbn(int variavel){
-        response = request.when().get(ENDPOINT_GET_BOOK_BY_ISBN + "/"+ variavel);
-        https://fakerestapi.azurewebsites.net/api/Books/200
-        System.out.println("response: " + response.prettyPrint());
+        requestPost = RestAssured.given().
+                request().
+                header("Content-Type", "application/json").
+                body(book.toJSONString());
 
     }
 
-    @When("I post")
-    public void iPost() {
-        response = request.when().post(ENDPOINT_GET_BOOK_BY_ISBN);
-        System.out.println("response: " + response.prettyPrint());
+    @When("I post new book")
+    public void i_post_new_book() {
+        responsePost = requestPost.when().post(ENDPOINT_GET_BOOK);
+        System.out.println("response: " + responsePost.prettyPrint());
     }
 
-    @Then("the status code is x")
-    public void verify_status_code(){
-        json = response.then().
-                            statusCode(200).
-                            assertThat().
-                            body("Title", Matchers.equalTo("Betania"));
+    @Then("^the book with title '(.*?)' was successfully created and statusCode return '(.*?)'$")
+    public void the_book_with_title_was_successfully_created_and_statusCode_return(String title, int statusCode) {
+          responsePost.then().
+            statusCode(statusCode).
+            assertThat().
+            body("Title", Matchers.equalTo(title));
     }
 
-//    /**
-//     * asserts on json fields with single values
-//     */
-//    @And("response includes the following$")
-//    public void response_equals(Map<String,String> responseFields){
-//        for (Map.Entry<String, String> field : responseFields.entrySet()) {
-//            if(StringUtils.isNumeric(field.getValue())){
-//                json.body(field.getKey(), CoreMatchers.equalTo(Integer.parseInt(field.getValue())));
-//            }
-//            else{
-//                json.body(field.getKey(), CoreMatchers.equalTo(field.getValue()));
-//            }
-//        }
-//    }
+    @Given("fetch active book")
+     public void fetch_active_book() {
+            requestGet = RestAssured.given().header("Content-Type", "application/json");
+    }
+
+    @When("^fetch book with id '(.*?)'$")
+    public void fetch_book_with_title(String id){
+        responseGet = requestGet.when().get(ENDPOINT_GET_BOOK + "/" + id);
+        https://fakerestapi.azurewebsites.net/api/Books/
+        System.out.println("response: " + responseGet.prettyPrint());
+    }
+
+    @Then("^the book with title '(.*?)' was successfully found")
+    public void the_book_with_title_was_successfully_found(String title) {
+        responseGet.then().statusCode(200);
+    }
 }
